e2f87b23b6c80e5178f31650fa09946dee9971bf
xiumubai <1547702880@qq.com>
2022-06-17 22:50:41 +0800

    feat: 完善静态服务器，使其基于 stream，并能给出正确的 Content-Length。

--- servers-fs.js
+++ servers-fs.js
@@ -2,15 +2,18 @@
  * @Author: 朽木白
  * @Date: 2022-06-17 22:27:49
  * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-06-17 22:35:15
+ * @LastEditTime: 2022-06-17 22:48:53
  * @Description:
  */
 const http = require('node:http');
 const fs = require('node:fs');
+const fsp = require('node:fs/promises');
 
 const html = fs.readFileSync('./index.html');
-const server = http.createServer((req, res) => {
-  res.end(html);
+const server = http.createServer(async (req, res) => {
+  const stat = await fsp.stat('./index.html');
+  res.setHeader('content-length', stat.size);
+  fs.createReadStream('./index.html').pipe(res);
 });
 
 server.listen(3000, () => {
