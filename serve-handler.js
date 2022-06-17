6a1cb05bdeba2ccffc1ad9a36a4849245b5ac682
xiumubai <1547702880@qq.com>
2022-06-17 22:52:48 +0800

    feat: serve-handler

---
+++ serve-handler.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2022-06-17 22:51:46
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-06-17 22:52:00
+ * @Description:
+ */
+// 高阶: 继续完善静态服务器，使其作为一个命令行工具，支持指定端口号、读取目录、404、stream (甚至 trailingSlash、cleanUrls、rewrite、redirect 等)。可参考 serve-handler
+// 参考：https://github.com/vercel/serve-handler
